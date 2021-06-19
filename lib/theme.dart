af1062248e3f1989a354efc4258d917bac9cc668
Rifqi Eka Hardianto <ekarifqi1@gmail.com>
2021-06-19 10:27:12 +0700

    update

--- lib/theme.dart
+++ lib/theme.dart
@@ -31,6 +31,10 @@ TextStyle redTextStyle = GoogleFonts.saira(
 );
 
 // NOTE: FONT WEIGHTS
-
 FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
 FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
